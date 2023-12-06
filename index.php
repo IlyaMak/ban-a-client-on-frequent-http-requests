263017246b7aaa3d6dd12b8ae1af0473bf6752aa
Ilya <ilmobdev@gmail.com>
2023-12-06 23:25:47 +0100

    Code modify

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
-
 session_start();
 
-define('MAX_REQUESTS', 3);
-define('MAX_BAN_TIME', 10);
+define('MAX_REQUESTS', 10);
+define('BAN_TIME_DURATION', 120);
+define('ALLOWED_IN_TIME_REQUESTS', 60);
 
-if (!isset($_SESSION['counter']) && !isset($_SESSION['retryTime'])) {
-    $_SESSION['counter'] = 1;
-    $_SESSION['retryTime'] = time();
+if (!isset($_SESSION['requestTimestamps'])) {
+    $_SESSION['requestTimestamps'] = [];
+}
+
+$_SESSION['requestTimestamps'] = array_filter(
+    $_SESSION['requestTimestamps'],
+    'getAllowedInTimeRequests'
+);
+
+// "- 1" in this condition to MAX_REQUESTS is needed because the current filtered request is not added yet
+if (
+    !isset($_SESSION['banTime'])
+    && count($_SESSION['requestTimestamps']) >= MAX_REQUESTS - 1
+) {
+    $_SESSION['banTime'] = time();
 }
 
 if (
-    $_SESSION['counter'] > MAX_REQUESTS &&
-    (isset($_SESSION['retryTime']) &&
-    time() - $_SESSION['retryTime'] >= MAX_BAN_TIME)
+    isset($_SESSION['banTime'])
+    && time() - $_SESSION['banTime'] < BAN_TIME_DURATION
 ) {
-    $_SESSION['counter'] = 0;
-    unset($_SESSION['retryTime']);
-}
-
-$_SESSION['counter']++;
-
-if ($_SESSION['counter'] > MAX_REQUESTS) {
-    if (!isset($_SESSION['retryTime'])) {
-        $_SESSION['retryTime'] = time();
-    }
-    $remainingBanTime =  $_SESSION['retryTime'] + MAX_BAN_TIME - time();
+    $remainingBanTime =  $_SESSION['banTime'] + BAN_TIME_DURATION - time();
     echo "You've done too many requests, please wait $remainingBanTime seconds to make new requests";
     http_response_code(429);
-    header("Retry-After:" . MAX_BAN_TIME);
+    header("Retry-After:" . BAN_TIME_DURATION);
 } else {
+    $_SESSION['requestTimestamps'][] = time();
+    unset($_SESSION['banTime']);
     echo 'Greetings';
 }
 
+function getAllowedInTimeRequests(int $requestTime)
+{
+    return time() - $requestTime < ALLOWED_IN_TIME_REQUESTS;
+}
